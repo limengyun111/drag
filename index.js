91d20adc281c57004ebe57b83aa8207d1cfe50ef
云 <oncwnuLGu9BAby_Iy8qKYwvxDV00@git.weixin.qq.com>
2025-07-21 11:35:47 +0800

    drag multi ele in range

--- index.js
+++ index.js
@@ -1,87 +1,120 @@
 class DraggableEle {
-	constructor(id) {
-		this.id = id;
-		this.ele = document.getElementById(this.id);
+  static paramEleHistory = new Set();
 
-	}
-	static beginMoveInfo = {
-		isBeginMove: false,
-		mouseBeginPos: undefined,
-		dragEleBeginPos: undefined
-	}
+  constructor(ele) {
+    this.ele = ele;
+    if (DraggableEle.paramEleHistory.has(this.ele)) {
+      return [...DraggableEle.paramEleHistory].find(item => item === this.ele);
+    }
+    DraggableEle.paramEleHistory.add(this.ele);
 
-	getDragElePos = () => {
-		const eleRect = this.ele.getBoundingClientRect();
-		return {
-			x: eleRect.width + eleRect.left,
-			y: eleRect.height + eleRect.top,
-			left: eleRect.left,
-			top: eleRect.top
-		}
-	};
-	isMouseInRange = (mousePos) => {
-		const elePos = this.getDragElePos();
-		return mousePos.x - elePos.left > 0 && mousePos.y - elePos.top && elePos.x - mousePos.x > 0 && elePos.y - mousePos.y > 0;
-	};
+    this.isBeginMove = false;
+    this.mouseBeginPos = undefined;
+    this.dragEleBeginPos = undefined;
+    this.moveRange = {
+      top: 100,
+      left: 100,
+      right: 700,
+      bottom: 600,
+    };
+    this.draggable();
 
-	#setMoveBeginVal = (posX, posY) => {
-		const elePos = this.getDragElePos();
-		this.beginMoveInfo = {
-			isBeginMove: true,
-			mouseBeginPos: {
-				x: posX,
-				y: posY,
-			},
-			dragEleBeginPos: {
-				x: elePos.left,
-				y: elePos.top
-			}
-
-		}
-	}
-	#setDragElePos = (curX, curY) => {
-		this.ele.style.left = this.beginMoveInfo.dragEleBeginPos.x + curX - this.beginMoveInfo.mouseBeginPos.x + 'px';
-		this.ele.style.top = this.beginMoveInfo.dragEleBeginPos.y + curY - this.beginMoveInfo.mouseBeginPos.y + 'px';
-	}
-
-	draggable = () => {
-		this.ele.addEventListener('touchstart', (event) => {
-			const touch = event.touches[0];
-			if (this.isMouseInRange({ x: touch.clientX, y: touch.clientY })) {
-				this.#setMoveBeginVal(touch.clientX, touch.clientY);
-			}
-		});
-		this.ele.addEventListener('mousedown', (event) => {
-			if (this.isMouseInRange(event)) {
-				this.#setMoveBeginVal(event.x, event.y);
-			}
-		});
-		document.addEventListener('mousemove', (event) => {
-			if (this.isMouseInRange(event)) {
-				this.ele.style.cursor = 'move';
-			} else {
-				this.ele.style.cursor = 'pointer'
-			};
-
-			if (this.beginMoveInfo.isBeginMove) {
-				this.#setDragElePos(event.x, event.y);
-			}
-		});
-		document.addEventListener('touchmove', (event) => {
-			const touch = event.touches[0];
-			if (this.beginMoveInfo.isBeginMove) {
-				this.#setDragElePos(touch.clientX, touch.clientY);
-			}
-		});
-
-		document.addEventListener('mouseup', (event) => {
-			this.beginMoveInfo = {...this.beginMoveInfo, isBeginMove: false }
-		});
-		document.addEventListener('touchend', (event) => {
-			this.beginMoveInfo = {...this.beginMoveInfo, isBeginMove: false }
-		});
-
-	}
+  }
 
 
+  getDragElePos = () => {
+    const eleRect = this.ele.getBoundingClientRect();
+    return {
+      // x: eleRect.width + eleRect.left,
+      // y: eleRect.height + eleRect.top,
+      // width: eleRect.width,
+      // height: eleRect.height,
+      left: eleRect.left,
+      top: eleRect.top,
+      right: eleRect.right,
+      bottom: eleRect.bottom
+    }
+  };
+  isMouseInRange = (mousePos) => {
+    const elePos = this.getDragElePos();
+    const isMouseInRange = mousePos.x - elePos.left > 0 && mousePos.y - elePos.top && elePos.right - mousePos.x > 0 && elePos.bottom - mousePos.y > 0
+    return isMouseInRange;
+  };
+
+  #setMoveBeginVal = (posX, posY) => {
+    const elePos = this.getDragElePos();
+
+    this.isBeginMove = true,
+      this.mouseBeginPos = {
+        x: posX,
+        y: posY,
+      },
+      this.dragEleBeginPos = {
+        // x: elePos.x,
+        // y: elePos.y,
+        left: elePos.left,
+        right: elePos.right,
+        top: elePos.top,
+        bottom: elePos.bottom
+      }
+  }
+  #setDragElePos = (mouseCurX, mouseCurY) => {
+    const eleCurX = this.dragEleBeginPos.left + mouseCurX - this.mouseBeginPos.x;
+    const eleCurY = this.dragEleBeginPos.top + mouseCurY - this.mouseBeginPos.y;
+
+    const getTranslateX = () => {
+      if (eleCurX <= this.moveRange.left) return this.moveRange.left;
+      if (eleCurX >= this.moveRange.right) return this.moveRange.right;
+      return eleCurX;
+    }
+    const getTranslateY = () => {
+      if (eleCurY <= this.moveRange.top) return this.moveRange.top;
+      if (eleCurY >= this.moveRange.bottom) return this.moveRange.bottom;
+      return eleCurY
+    }
+
+
+    this.ele.style.transform = `translate(${getTranslateX()}px, ${getTranslateY()}px)`;
+  }
+
+
+  draggable = () => {
+    this.ele.addEventListener('touchstart', (event) => {
+      const touch = event.touches[0];
+      if (this.isMouseInRange({ x: touch.clientX, y: touch.clientY })) {
+        this.#setMoveBeginVal(touch.clientX, touch.clientY);
+      }
+    });
+    this.ele.addEventListener('mousedown', (event) => {
+      if (this.isMouseInRange(event)) {
+        this.#setMoveBeginVal(event.x, event.y);
+      }
+    });
+
+    document.addEventListener('mousemove', (event) => {
+      if (this.isMouseInRange(event)) {
+        this.ele.style.cursor = 'move';
+        if (this.isBeginMove) {
+          this.#setDragElePos(event.x, event.y);
+        }
+      } else {
+        this.ele.style.cursor = 'pointer'
+      };
+    });
+    document.addEventListener('touchmove', (event) => {
+      event.preventDefault();
+      const touch = event.touches[0];
+      if (this.isBeginMove) {
+        this.#setDragElePos(touch.clientX, touch.clientY);
+      }
+    }, { passive: false });
+
+    document.addEventListener('mouseup', (event) => {
+      this.isBeginMove = false;
+    });
+    document.addEventListener('touchend', (event) => {
+      this.isBeginMove = false
+    });
+
+  }
 }
